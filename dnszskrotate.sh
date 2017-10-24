4a97ee91c67e2c6836737f63f20038486e21d872
Max Kostikov <max@kostikov.co>
2017-10-24 15:34:02 +0200

    Update dnszskrotate.sh
    relative path fix

--- dnszskrotate.sh
+++ dnszskrotate.sh
@@ -11,7 +11,7 @@
 
 ## Settings
 # read settings
-mypath=`dirname $0`
+mypath=`dirname $0 | xargs realpath`
 if [ ! -f ${mypath}/dnstools.cf ]
 then
         echo "Configuration file dnstools.cf not found!"
