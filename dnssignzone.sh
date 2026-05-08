0131c4789ebe262d131070af53078656f61c5b95
Max Kostikov <max@kostikov.co>
2026-05-08 09:55:38 +0100

    Support modern DNSSEC best parctices
    Change NSEC3 params; better support for RRSIG validity intervals

--- dnssignzone.sh
+++ dnssignzone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Sign DNS zone using DNSSEC keys
-# v.20170531 (c)2016-2017 by Max Kostikov http://kostikov.co e-mail: max@kostikov.co
+# v.20260705 (c)2016-2026 by Max Kostikov W: http://kostikov.co E: max@kostikov.co
 #
 # Usage: dnssignzone.sh /path/to/zone/file
 
@@ -9,24 +9,21 @@
 # Requires ldns toolset
 
 # Check arguments
-if [ $# -ne 1 ]
-then
+if [ $# -ne 1 ]; then
         echo "Provide /path/to/zone/file as argument!"
         exit 1
 fi
 
 # is zone file exist?
-if [ ! -f $1 ]
-then
+if [ ! -f $1 ]; then
         echo "Zone file $1 not found!"
         exit 1
 fi
 
 ## Setttings
 # read settings
-mypath=`dirname $0 | xargs readlink -f`
-if [ ! -f ${mypath}/dnstools.cf ]
-then
+mypath=`dirname $0 | xargs realpath`
+if [ ! -f ${mypath}/dnstools.cf ]; then
         echo "Configuration file dnstools.cf not found!"
         exit 1
 else
@@ -34,26 +31,25 @@ else
 fi
 
 # prepare parameters
-zonedir=`dirname $1`                    # path to zone dir
+zonedir=$(dirname $1)                   # path to zone dir
 
 # Zone is DNSSEC
-if [ `ls $zonedir/*.key | wc -l` -eq 0 ]
-then
+if [ $(ls $zonedir/*.key | wc -l) -eq 0 ]; then
         echo "Zone have not any keys!"
         exit 1
 fi
 
-sigopt="-n -t 100 -s `head -c 512 /dev/random | shasum | cut -b 1-16`"
+# sigopt="-n -p -t 100 -s `head -c 512 /dev/random | shasum | cut -b 1-16`"
+sigopt="-n -t 0 -i $(date -u -v-86400S +%Y%m%d%H%M%S) -e $(date -u -v+${arcage}S +%Y%m%d%H%M%S)"
                                         # zone signing options
 
 ## Get actual keys list
-ZSK=`find $zonedir/*.key -mtime -${arcage}s -exec grep -il '256 3' {} \; | xargs ls -tr | head -1 | sed 's/\.key$//'`
+ZSK=$(find $zonedir/*.key -mtime -${arcage}s -exec grep -il '256 3' {} \; | xargs ls -tr | head -1 | sed 's/\.key$//')
                                         # get oldest existing ZSK
-KSK=`find $zonedir/*.key -exec egrep -il '(257|385) 3' {} \; | sed 's/\.key$//'`
+KSK=$(find $zonedir/*.key -exec egrep -il '(257|385) 3' {} \; | sed 's/\.key$//')
                                         # get all existing KSK
 
-if [ -z "$ZSK" -o -z "$KSK" ]
-then
+if [ -z "$ZSK" -o -z "$KSK" ]; then
         echo "Full keys set not found in $zonedir !"
         exit 1
 fi
