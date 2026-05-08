e4992dd852446b65f8e3a74ef420e9f9c37cdea3
Max Kostikov <max@kostikov.co>
2026-05-08 09:59:33 +0100

    Better DNS zone format support

--- dnsnewserial.sh
+++ dnsnewserial.sh
@@ -1,41 +1,37 @@
 #!/bin/sh
 
 # Change DNS zone serial in YYYYMMDDNN format
-# v.20180401 (c)2016-18 by Max Kostikov http://kostikov.co e-mail: max@kostikov.co
+# v.20260705 (c)2016-2026 by Max Kostikov W: http://kostikov.co E: max@kostikov.co
 #
 # Usage: dnsnewserial.sh /path/to/zone/file
 #
 
 # Check arguments
-if [ $# -ne 1 ]
-then
+if [ $# -ne 1 ]; then
         echo "Provide /path/to/zone/file as argument!"
         exit 1
 fi
 
 # is zone file exist?
-if [ ! -f $1 ]
-then
+if [ ! -f $1 ]; then
         echo "Zone file $1 not found!"
         exit 1
 fi
 
 # get current serial
-curser=`cat $1 | awk -v RS='@' '$1 == "IN" && $2 == "SOA" { print $6 }'`
+curser=$(cat $1 | awk -v RS='@' '$1 == "IN" && $2 == "SOA" { print $6 }')
 
-if [ -z "$curser" ]
-then
+if [ -z "$curser" ]; then
         echo "Serial not found in $1 file!"
         exit 1
 fi
 
-newser=`date '+%Y%m%d'`
+newser=$(date '+%Y%m%d')
 
-if [ `expr $curser : $newser` = 8 ]     # if serial today was already changed
-then
-        newser=`expr $curser + 1`       # increment it
+if [ $(expr $curser : $newser) = 8 ]; then
+        newser=$(expr $curser + 1)      # if serial have the same date increment it
 else
-        newser=${newser}00              # else assign new
+        newser=${newser}00              # else assing a new one
 fi
 
 # write new serial to zone file
